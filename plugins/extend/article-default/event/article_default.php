dc6cc50d2b479bea2a24ade94f4c007b86bcdd8e
72tlukas72 <149176237+72tlukas72@users.noreply.github.com>
2023-11-05 11:00:58 +0100

    Update article_default.php

--- plugins/extend/article-default/event/article_default.php
+++ plugins/extend/article-default/event/article_default.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace Sunlight;
-
-use SunlightExtend\ArticleDefault;
-
 return function (array $args) {
     $args['data']['confirmed'] = $this->getConfig()['approved'];
     $args['data']['rateon'] = $this->getConfig()['rating'];
